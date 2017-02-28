cd2b8a605112d16b2c00f01f78a2296675c29276
Arnelle Balane <arnellebalane@gmail.com>
2017-02-28 14:17:34 +0800

    dont copy secrets.json from flightplan

--- generators/flightplan/index.js
+++ generators/flightplan/index.js
@@ -8,17 +8,6 @@ module.exports = class extends Generator {
       this.templatePath('flightplan.js'),
       this.destinationPath('flightplan.js')
     );
-
-    const config = this.fs.extendJSON(
-      this.destinationPath('config/secrets.json'),
-      {
-        "PRODUCTION_HOST": "",
-        "PRODUCTION_USERNAME": "",
-        "PRODUCTION_PRIVATE_KEYFILE": "",
-        "PRODUCTION_PROJECT_PATH": "",
-        "PRODUCTION_PROCESS_NAME": ""
-      }
-    );
   }
 
 
