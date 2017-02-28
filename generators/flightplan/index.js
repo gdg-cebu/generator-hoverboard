d45ab75a3650a432ee149263d7a939b5615763ac
Arnelle Balane <arnellebalane@gmail.com>
2017-02-28 14:18:57 +0800

    undo previous version changes

--- generators/flightplan/index.js
+++ generators/flightplan/index.js
@@ -8,6 +8,17 @@ module.exports = class extends Generator {
       this.templatePath('flightplan.js'),
       this.destinationPath('flightplan.js')
     );
+
+    const config = this.fs.extendJSON(
+      this.destinationPath('config/secrets.json'),
+      {
+        "PRODUCTION_HOST": "",
+        "PRODUCTION_USERNAME": "",
+        "PRODUCTION_PRIVATE_KEYFILE": "",
+        "PRODUCTION_PROJECT_PATH": "",
+        "PRODUCTION_PROCESS_NAME": ""
+      }
+    );
   }
 
 
